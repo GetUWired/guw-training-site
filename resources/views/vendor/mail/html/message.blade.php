e6d379046acb1ac2576c572113552408c560f9fd
Garrett <nova4005@gmail.com>
2018-04-15 20:19:26 -0400

    update email template

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -3,7 +3,7 @@
     @slot('header')
         @component('mail::header', ['url' => config('app.url')])
             <img src="{{ secure_asset('images/GUW-new-logo-white-with-blue-background_DEVELOPERS.jpg') }}" alt="{{ config('app
-            .name') }}" width="100%" height="auto">
+            .name') }}" width="300px" height="auto" style="margin:0 auto;display:block;">
         @endcomponent
     @endslot
 
