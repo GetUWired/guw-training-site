4b56eb5afe393ba267c8a4d72bb96cc406e8513a
Garrett <nova4005@gmail.com>
2018-04-16 21:32:59 -0400

    admin protect routes

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,10 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/leaderboard', 'ProblemController@leaders')->name('leaders');
 
 //Forms
-Route::get('/add-problem', 'ProblemController@create')->name('createproblem')->middleware('auth');
-Route::post('/save-problem', 'ProblemController@store')->name('saveproblem');
-Route::get('/edit/{id}', 'ProblemController@edit')->name('editproblem')->middleware('auth');
-Route::patch('/edit/{id}', 'ProblemController@update')->name('updateproblem')->middleware('auth');
+Route::get('/add-problem', 'ProblemController@create')->name('createproblem')->middleware('admin');
+Route::post('/save-problem', 'ProblemController@store')->name('saveproblem')->middleware('admin');
+Route::get('/edit/{id}', 'ProblemController@edit')->name('editproblem')->middleware('admin');
+Route::patch('/edit/{id}', 'ProblemController@update')->name('updateproblem')->middleware('admin');
 Route::post('/problems/problem-completion', 'UserProblemController@store')->name('completeproblem');
 Route::get('/problems/problem-completion-check', 'UserProblemController@problem_status')->name('problemstatus');
 
@@ -36,10 +36,10 @@ Route::post('/problem/search', "ProblemController@search")->name('problem.search
 
 //Route::resource('sets', 'SetController')->middleware('auth');
 Route::get('/sets', 'SetController@index')->middleware('auth');
-Route::get('/sets/create', 'SetController@create')->middleware('auth')->name('setcreate');
-Route::post('/sets/store', 'SetController@store')->middleware('auth')->name('setstore');
-Route::get('/sets/edit/{set}', 'SetController@edit')->middleware('auth')->name('setedit');
-Route::post('/sets/update/{set}', 'SetController@update')->middleware('auth')->name('setupdate');
+Route::get('/sets/create', 'SetController@create')->middleware('admin')->name('setcreate');
+Route::post('/sets/store', 'SetController@store')->middleware('admin')->name('setstore');
+Route::get('/sets/edit/{set}', 'SetController@edit')->middleware('admin')->name('setedit');
+Route::post('/sets/update/{set}', 'SetController@update')->middleware('admin')->name('setupdate');
 Route::get('/sets/{id}', 'SetController@show')->middleware('auth')->name('singleset');
 Route::post('/sets/problem-completion', 'UserProblemController@store')->name('completeproblem');
 Route::post('/problem/problem-completion', 'UserProblemController@store')->name('completeproblem');
