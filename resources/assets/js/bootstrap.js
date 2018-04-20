cda49df3c7fa7b42a441b705d9b4e2cef0908985
Garrett <nova4005@gmail.com>
2018-04-19 21:25:35 -0400

    install summernote

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -9,8 +9,9 @@ window._ = require('lodash');
 
 try {
     window.$ = window.jQuery = require('jquery');
-
     require('bootstrap-sass');
+    require('summernote/dist/summernote.css');
+    require('summernote');
 } catch (e) {}
 
 /**
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,12 @@
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
           integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     
+    <!-- Include Editor style. -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/froala-editor/2.5.1/css/froala_editor.pkgd.min.css" rel="stylesheet"
+          type="text/css"/>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/froala-editor/2.5.1/css/froala_style.min.css" rel="stylesheet"
+          type="text/css"/>
+    
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     
@@ -17,92 +23,97 @@
     <script src="{{ asset('js/sweetalert.min.js') }}"></script>
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-                    
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                            data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'GUW Training') }}
-                    </a>
-                </div>
+<div id="app">
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
                 
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        @auth
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                                   aria-expanded="false">Languages <span class="caret"></span></a>
-                                <ul class="dropdown-menu" role="menu">
-                                    <li><a href="{{ url('/problems/php') }}">PHP</a></li>
-                                    <li><a href="{{ url('/problems/jquery') }}">jQuery</a></li>
-                                    <li><a href="{{ url('/problems/javascript') }}">Javascript</a></li>
-                                    <li><a href="{{ url('/problems/html') }}">HTML</a></li>
-                                    <li><a href="{{ url('/problems/css') }}">CSS</a></li>
-                                    <li><a href="{{ url('/problems/sql') }}">SQL</a></li>
-                                    <li><a href="{{ url('/problems/json') }}">JSON</a></li>
-                                    <li><a href="{{ url('/problems/xml') }}">XML</a></li>
-                                    <li><a href="{{ url('/problems/infusionsoft') }}">Infusionsoft</a></li>
-                                    <li><a href="{{ url('/problems/wordpress') }}">WordPress</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="/sets">Problem Sets</a></li>
-                            <li><a href="/leaderboard">Leaderboard</a></li>
-                        @endauth
-                    </ul>
-                    
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                                   aria-expanded="false">
-                                    <img class="avatar" src="https://api.adorable.io/avatars/25/<?= str_random(15);?>.png"
-                                         alt="Avatar"> {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-                                <ul class="dropdown-menu" role="menu">
-                                    @if(Auth::user()->user_level == 10)
-                                        <li><a href="/add-problem">Add Problem</a></li>
-                                        <li><a href="/sets/create">Add Set</a></li>
-                                        <li><a href="{{ route('register') }}">Register</a></li>
-                                    @endif
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                           onclick="event.preventDefault();
-                                                         document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-                                        
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                              style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                
+                <!-- Branding Image -->
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'GUW Training') }}
+                </a>
             </div>
-        </nav>
-        @yield('content')
-    </div>
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-    @include('layouts.flash-message')
+            
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                <!-- Left Side Of Navbar -->
+                <ul class="nav navbar-nav">
+                    @auth
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                               aria-expanded="false">Languages <span class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{ url('/problems/php') }}">PHP</a></li>
+                                <li><a href="{{ url('/problems/jquery') }}">jQuery</a></li>
+                                <li><a href="{{ url('/problems/javascript') }}">Javascript</a></li>
+                                <li><a href="{{ url('/problems/html') }}">HTML</a></li>
+                                <li><a href="{{ url('/problems/css') }}">CSS</a></li>
+                                <li><a href="{{ url('/problems/sql') }}">SQL</a></li>
+                                <li><a href="{{ url('/problems/json') }}">JSON</a></li>
+                                <li><a href="{{ url('/problems/xml') }}">XML</a></li>
+                                <li><a href="{{ url('/problems/infusionsoft') }}">Infusionsoft</a></li>
+                                <li><a href="{{ url('/problems/wordpress') }}">WordPress</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="/sets">Problem Sets</a></li>
+                        <li><a href="/leaderboard">Leaderboard</a></li>
+                    @endauth
+                </ul>
+                
+                <!-- Right Side Of Navbar -->
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- Authentication Links -->
+                    @guest
+                        <li><a href="{{ route('login') }}">Login</a></li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                               aria-expanded="false">
+                                <img class="avatar" src="https://api.adorable.io/avatars/25/<?= str_random(15);?>.png"
+                                     alt="Avatar"> {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu" role="menu">
+                                @if(Auth::user()->user_level == 10)
+                                    <li><a href="/add-problem">Add Problem</a></li>
+                                    <li><a href="/sets/create">Add Set</a></li>
+                                    <li><a href="{{ route('register') }}">Register</a></li>
+                                @endif
+                                <li>
+                                    <a href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                         document.getElementById('logout-form').submit();">
+                                        Logout
+                                    </a>
+                                    
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                          style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
+                    @endguest
+                </ul>
+            </div>
+        </div>
+    </nav>
+    @yield('content')
+</div>
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}"></script>
+<script>
+    $(document).ready(function () {
+        $('textarea').summernote()
+    })
+</script>
+@include('layouts.flash-message')
 </body>
 </html>
